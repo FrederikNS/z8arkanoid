709f9fb77708e450003ab3f48bd53d634a151323
FrederikNS <FrederikNS@dc021d51-b64f-0410-b550-5dcd8fc4b7f8>
2008-06-19 14:25:47 +0000

    Blocks.h!

--- trunk/game/blocks.h
+++ trunk/game/blocks.h
@@ -6,11 +6,13 @@
 
 void blocks_clear(void);
 //local function
-//char block_coords_in_area(int x, int y);
-//unsigned char* block_on(int x, int y);
-//unsigned char block_value_on(int x, int y);
-//void block_setvalue(int x, int y, int value);
-//void destroy_block(int x, int y);
+char block_coords_in_area(int x, int y);
+unsigned char* block_on(int x, int y);
+unsigned char block_value_on(int x, int y);
+void block_setvalue(int x, int y, int value);
+void block_draw(int x, int y);
+void block_draw_all(void);
+void destroy_block(int x, int y);
 char block_hit(int x, int y);
 
 #endif
