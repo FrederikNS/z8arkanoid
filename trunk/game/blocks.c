65515e7dc1775c02bdfb96925a0ac67cb3c9e91d
FrederikNS <FrederikNS@dc021d51-b64f-0410-b550-5dcd8fc4b7f8>
2008-06-22 18:44:48 +0000

    rrr

--- trunk/game/blocks.c
+++ trunk/game/blocks.c
@@ -3,7 +3,7 @@
 
 //Does not work, fuckings to the precompiler
 //unsigned char board[BOARD_WIDTH*BOARD_HEIGHT*2/8];
-unsigned char blocks[BLOCKS_WIDTH][BLOCKS_HEIGHT];
+unsigned char blocks[BLOCKS_WIDTH*BLOCKS_HEIGHT];
 
 enum BLOCK_TYPES_ {
 	NO_BLOCK,
@@ -17,11 +17,13 @@ enum BLOCK_TYPES_ {
 	HARD_BLOCK_3
 } BLOCK_TYPES;
 
+
+
 void blocks_clear(void) {
 	int x, y;
 	for(x=0;x<BLOCKS_WIDTH;x++){
 		for(y=0;y<BLOCKS_HEIGHT;y++){
-			blocks[x][y] = 0;
+			blocks[x+y*BLOCKS_WIDTH] = 0;
 		}
 	}
 }
@@ -41,7 +43,7 @@ unsigned char* block_on(int x, int y) {
 	//x -= 1; //1 character gap between blocks and walls
 	//y -= 1; 
 	if(block_coords_in_area(x, y))
-		return &blocks[x][y];
+		return &blocks[x+y*BLOCKS_WIDTH];
 	return 0;
 }
 
@@ -102,7 +104,7 @@ void block_draw(char x, char y){
 void block_draw_all() {
 	int x;
 	int y;
-	for(x=0;xBLOCKS_WIDTH;x++){
+	for(x=0;x<BLOCKS_WIDTH;x++){
 		for(y=0;y<BLOCKS_HEIGHT;y++){
 			block_draw(x,y);
 		}
@@ -119,6 +121,10 @@ void block_destroy(int x, int y) {
 //1 - normal block, bounce off it
 //2 - the block was destroyed! Bounce!
 
+char block_hit_fixed(int x, int y){
+return block_hit(x>>8,
+}
+
 char block_hit_screen_coord(int x, int y){
 	return block_hit((x-3)>>1,y-3);
 }
@@ -127,6 +133,7 @@ char block_hit(int x, int y) {
 	unsigned char* block = block_on(x, y);
 	if(!block || !*block) return 0; //Either the block is empty, or the coords are out of range, so no need for the ball to bounce off.
 	switch(block_value_on(x,y)){
+		case HARD_BLOCK_1:
 		case REGULAR_BLOCK:
 			block_destroy(x,y);
 			break;
@@ -152,15 +159,14 @@ char block_hit(int x, int y) {
 			break;
 		case INDESTRUCTIBLE_BLOCK:
 			break;
-		case HARD_BLOCK_1:
-			block_destroy(x,y);
-			break;
 		case HARD_BLOCK_2:
 			block_set_value(x,y,HARD_BLOCK_1);
 			break;
 		case HARD_BLOCK_3:
 			block_set_value(x,y,HARD_BLOCK_2);
 			break;
+		case FAKE_BLOCK:
+			block_destroy(x,y);
 		default:
 			return 0;
 			break;
