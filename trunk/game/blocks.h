9ac13728547f299d2d3c07d9360cff9ddbcdf959
FrederikNS <FrederikNS@dc021d51-b64f-0410-b550-5dcd8fc4b7f8>
2008-06-23 20:13:46 +0000

    Woot, i suck at coding at 22:13 in the evening

--- trunk/game/blocks.h
+++ trunk/game/blocks.h
@@ -5,9 +5,14 @@
 #define BLOCKS_HEIGHT 10
 
 void blocks_clear(void);
+void block_counter(void)
+int blocks_left(void);
 void blocks_draw(void);
 void blocks_loadlevel(int lvl);
 char block_hit(int x, int y);
 char block_hit_fixed(int x, int y);
+char block_coords_in_area(int x, int y)
+unsigned char* block_on(int x, int y)
+char block_hit_coord(int x, int y)
 
 #endif
