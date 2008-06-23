d6208e0ca2aaccc916612b726d288490f0f50cd6
FrederikNS <FrederikNS@dc021d51-b64f-0410-b550-5dcd8fc4b7f8>
2008-06-23 14:54:34 +0000

    buttons retrun 1 if pressed, 0 if not

--- HLI/button.c
+++ HLI/button.c
@@ -4,28 +4,30 @@ void z_buttons_init(){
 	PDADDR = 0x01;
 	PDCTL |= 0xF;
 	PDADDR = 0;
-
+	
 	PFADDR = 0x01;
 	PFCTL |= 0xF;
 	PFADDR = 0;
 }
 
-char z_buttons_read(){
-	return ~(((0x80 & PFIN)>>7)|((0x40 & PFIN)>>5)|((0x8 & PDIN)>>1));
-}
-
 char z_button_right(void)
 {
-	return z_buttons_read()&1;
+	if(0x80==(0x80&PFIN))
+		return 1;
+	return 0
 }
 
 char z_button_middle(void)
 {
-	return (z_buttons_read()&2);
+	if(0x40==0x40&PFIN)
+		return 1;
+	return 0;
 }
 
 char z_button_left(void)
 {
-	return (z_buttons_read()&4);
+	if(0x8==0x8&PDIN)
+		return 1;
+		return 0;
 }
 
